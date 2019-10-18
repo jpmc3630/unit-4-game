4732cac25ff2f72c26e3c88ec87386f5cb8ff657
James McGlone <jamespmcglone@gmail.com>
2019-10-19 02:25:40 +0800

    Complete game

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # unit-4-game
+
+This is my ultimate animal fighter game.
+
+## Getting started
+
+Just choose an animal to play as.
+Then choose your enemy. 
+
+Clock attack, attack, attack
+Some possible outcomes can occur!
+
+Who will be the ultimate fighter?
+Nobody knows!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to Avi for laying out the structure of using an object model for me. 
+And also curse Avi for the pain this caused me for the last 15 hours.
+
+* Inspiration came from ultimate fighter memes!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,21 @@
 let myGame;
 let idCounter = -1;
 
-
-function createCharacter(name, image, health, atp, ctp) {
+function createCharacter(name, image, health, atp) {
     idCounter++;
   return {
     id : idCounter,
     name : name,
     image: image,
     health: health,
-    attackPower : atp,
-    counterPower : ctp
+    attackPower : atp
   }
   
 }
 
 function reset() {
-    
+  idCounter = -1;
+
   myGame = {
     allCharacters : [],
     remainingEnemies : [],
@@ -25,10 +24,10 @@ function reset() {
     gameStage : 0,
     fightRound : 0
   }
-  myGame.allCharacters.push(createCharacter("Boba Fett","https://via.placeholder.com/100",100,10,5));
-  myGame.allCharacters.push(createCharacter("Darth Vader","https://via.placeholder.com/100",150,20,2));
-  myGame.allCharacters.push(createCharacter("Obi Wan","https://via.placeholder.com/100",50,5,50));
-  myGame.allCharacters.push(createCharacter("Luke","https://via.placeholder.com/100", 125,15,40));
+  myGame.allCharacters.push(createCharacter("Dog","./assets/images/dog.png",100,16));
+  myGame.allCharacters.push(createCharacter("Monkey","./assets/images/monkey.png",80,7));
+  myGame.allCharacters.push(createCharacter("Turtle","./assets/images/turtle.png",140,10));
+  myGame.allCharacters.push(createCharacter("Cat","./assets/images/cat.png", 90,8));
 
 }
 
@@ -42,34 +41,37 @@ function getCharacterBlock(character) {
   `
 }
 
-function display() {
+function display(statusBar) {
   var allCharactersDiv = $("#all-characters");
   var myCharacterDiv = $("#my-character");
   var OpponentDiv = $("#enemy-characters");
-  
+  var attackButtonDiv = $("#attack-button");
 
   var myChar = myGame.allCharacters[myGame.myCharacter];
   var theOpp = myGame.allCharacters[myGame.theOpponent];
 
+  // Clear game divs
   allCharactersDiv.html("");
   myCharacterDiv.html("");
   OpponentDiv.html("");
+  attackButtonDiv.html("");
 
+  console.clear();
+console.log(myGame.gameStage);
 
-
+  //Update the statusbar
+  $("footer").html(statusBar);
   
-if (myGame.gameStage == 0) {
-    //GAME STAGE = 0
-
-  // draws all characters
-  for (let i = 0; i < myGame.allCharacters.length; i++) {
-    let character = myGame.allCharacters[i];
-     allCharactersDiv.append(getCharacterBlock(character));
-  }
+  //GAME STAGE = 0
+  if (myGame.gameStage == 0) {
+    
+    // draws all characters
+    for (let i = 0; i < myGame.allCharacters.length; i++) {
+      let character = myGame.allCharacters[i];
+      allCharactersDiv.append(getCharacterBlock(character));
+    }
   
-} 
-
-else if (myGame.gameStage == 1) {
+  } else if (myGame.gameStage == 1) {
     //GAME STAGE = 1, Player has been chosen
     
     // DRAW MY PLAYER 
@@ -81,11 +83,10 @@ else if (myGame.gameStage == 1) {
       let enemyChar = myGame.allCharacters[myGame.remainingEnemies[i]];
         
       allCharactersDiv.append(getCharacterBlock(enemyChar));
-      };
+    };
 
 
-
-} else if (myGame.gameStage == 2) {
+  } else if (myGame.gameStage == 2) {
   
     //GAME STAGE = 2, First Opponent has been chosen
         // DRAW MY PLAYER 
@@ -104,42 +105,80 @@ else if (myGame.gameStage == 1) {
         
     // create attack button
           let attackButton = $("<button>");
-          attackButton.text('Attack');
+          attackButton.text('Attack!');
           attackButton.addClass('attack-button');
-          OpponentDiv.append(attackButton);
+          attackButtonDiv.append(attackButton);
            
-
-
-
-
-
-
-    
+    } else if (myGame.gameStage == 3) {
+      //GAME STAGE = 3, Player has LOST
       
+       // DRAW MY OPONENT
+       OpponentDiv.html(getCharacterBlock(theOpp));
+
+      // draw ENEMEY PLAYERS if any left
+      for (let i = 0; i < myGame.remainingEnemies.length; i++) {
+          
+        let enemyChar = myGame.allCharacters[myGame.remainingEnemies[i]];
+          
+        allCharactersDiv.append(getCharacterBlock(enemyChar));
+      };
+
+        //append Rematch button
+        let rematchButton = $("<button>");
+        rematchButton.text('Rematch!');
+        rematchButton.addClass('rematch-button');
+        attackButtonDiv.append(rematchButton);
+  
+  
+    } else if (myGame.gameStage == 4) {
+      //GAME STAGE = 1, Player has WON
+      
+      // DRAW MY PLAYER 
+      myCharacterDiv.html(getCharacterBlock(myChar));
+  
+      //append Rematch button
+      let rematchButton = $("<button>");
+      rematchButton.text('Rematch!');
+      rematchButton.addClass('rematch-button');
+      attackButtonDiv.append(rematchButton);
+
+    } else if (myGame.gameStage == 5) {
+      //GAME STAGE = 5, Double knock out
+      
+      // draw ENEMEY PLAYERS if any left
+      for (let i = 0; i < myGame.remainingEnemies.length; i++) {
+    
+        let enemyChar = myGame.allCharacters[myGame.remainingEnemies[i]];
+          
+        allCharactersDiv.append(getCharacterBlock(enemyChar));
+      };
+
+      //append Rematch button
+      let rematchButton = $("<button>");
+      rematchButton.text('Rematch!');
+      rematchButton.addClass('rematch-button');
+      attackButtonDiv.append(rematchButton);
+
+    };
 
 
-// } else if (myGame.gameStage == 3) {
 
-
-//   // Choose next opponent
 
 
 };
 
 
 
-console.clear();
-console.log(myGame);
-  
-
-}
-
-
-
 $(document).ready(function() {
 
   reset();
-  display();
+  display(`Choose your Fighter!`);
+
+
+  $('body').on('click', '.rematch-button', function (){
+    reset();
+    display(`Choose your Fighter!`);
+  });
 
 
 
@@ -149,36 +188,59 @@ $(document).ready(function() {
 
     if (myGame.gameStage == 2) { //game stage is THE FIGHT
       
+      // increment fight round for attack multiplier
       myGame.fightRound++;
 
-      // set attach with mutliplier, and counter attack power
+      // set attack with mutliplier, and counter attack power
       let myAttack = (myGame.fightRound) * (myGame.allCharacters[myGame.myCharacter].attackPower);
       let counterAttack = myGame.allCharacters[myGame.theOpponent].attackPower;
       
        // subtract them from the the object model
       myGame.allCharacters[myGame.theOpponent].health -= myAttack;
       myGame.allCharacters[myGame.myCharacter].health -= counterAttack;
+
       // update status bar 
-      $("header").html(`You attacked <b>${myGame.allCharacters[myGame.theOpponent].name}</b> for ${myAttack} damage.<BR> <b>${myGame.allCharacters[myGame.theOpponent].name}</b> attacked you for ${counterAttack} damage.`);
+      display(`You attacked <b>${myGame.allCharacters[myGame.theOpponent].name}</b> for ${myAttack} damage.<BR> <b>${myGame.allCharacters[myGame.theOpponent].name}</b> attacked you for ${counterAttack} damage.`);
       
-      
-      if (myGame.allCharacters[myGame.theOpponent].health <= 0) {
-        if (myGame.remainingEnemies.length > 0) {
-          $("header").html(`You have defeated <b>${myGame.allCharacters[myGame.theOpponent].name}</b><BR>Choose your next opponent!`);
-          myGame.gameStage = 1; //go back to pick next opponent
-        } else {
-          alert('youv bloody won mate');
-          // you have won
-        };
+
+      // all potential end game states are below
+
+      // double knockout various scenarios for extra fun
+      if (myGame.allCharacters[myGame.myCharacter].health <= 0 && myGame.allCharacters[myGame.theOpponent].health <= 0) {
+        myGame.gameStage = 5;
+        if (myGame.remainingEnemies.length == 2) {
+          display(`It's a double knockout.<BR><b>${myGame.allCharacters[myGame.remainingEnemies[0]].name}</b> and <b>${myGame.allCharacters[myGame.remainingEnemies[1]].name}</b> win by default!<BR> Click rematch to play again!`)
         
-       
+          } else if (myGame.remainingEnemies.length == 1) {
+            display(`It's a double knockout.<BR><b>${myGame.allCharacters[myGame.remainingEnemies[0]].name}</b> wins by default!<BR>Click rematch to play again!`)
+        
+          } else {
+          display(`It's a double knockout. You and <b>${myGame.allCharacters[myGame.theOpponent].name}</b> have defeated each other.<BR>  Click rematch to play again!`)
+        };
+
+      } else if (myGame.allCharacters[myGame.myCharacter].health <= 0) {
+        // you have LOST
+        myGame.gameStage = 3; // losing game stage
+        display(`You have been defeated by <b>${myGame.allCharacters[myGame.theOpponent].name}</b>.<BR> Click rematch to play again!`)
+      
+      } else if (myGame.allCharacters[myGame.theOpponent].health <= 0) {
+        if (myGame.remainingEnemies.length > 0) {
+          
+          myGame.gameStage = 1; //go back to pick next opponent
+          display(`You have defeated <b>${myGame.allCharacters[myGame.theOpponent].name}</b><BR>Choose your next opponent!`);
+          
+        } else {
+          myGame.gameStage = 4; //winning game stage
+          display(`You have defeated all of your enemies. <b>You are victorious!!</b><BR>Click rematch button to play again!`); 
+          
+        }; 
       };
   
     };
 
 
 
-    display();
+    // display();
 
 
 
@@ -203,7 +265,7 @@ $(document).ready(function() {
 
           myGame.gameStage = 1;
           // update Status bar
-          $("header").html(`You have chosen <b>${myGame.allCharacters[myGame.myCharacter].name}</b>. Now choose which enemy to fight first!`);
+          display(`You have chosen <b>${myGame.allCharacters[myGame.myCharacter].name}</b>. Now choose your opponent!`);
       
       
         } else if (myGame.gameStage==1) { // game stage is choose first enemy
@@ -222,22 +284,17 @@ $(document).ready(function() {
               myGame.gameStage = 2; 
 
               // update Status bar
-              $("header").html(`You have chosen to fight <b>${myGame.allCharacters[myGame.theOpponent].name}</b>!`);
+              display(`You have chosen to fight <b>${myGame.allCharacters[myGame.theOpponent].name}</b>!<BR>Click the button to attack!`);
+              
     
 
           };
 
       };
 
-
-
-      display();
-
-
+      
 
   });
-
-
 
 
 });
